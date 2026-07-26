7b43725890791e02c6fa263b2f4671f1e2053d08
windsorroyalapps <137676262+windsorroyalapps@users.noreply.github.com>
2026-07-26 22:04:16 +1000

    Fix settings.gradle.kts: use Kotlin 2.0.21 for compose plugin support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,8 +18,8 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
 
 rootProject.name = "BMWE60CoderPro"
