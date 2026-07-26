4ab6a1f704456197979888fc397aa39a04c5366e
windsorroyalapps <137676262+windsorroyalapps@users.noreply.github.com>
2026-07-26 21:57:28 +1000

    Fix settings.gradle.kts: add plugin versions for AGP and Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,5 +16,11 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
+}
+
 rootProject.name = "BMWE60CoderPro"
 include(":app")
